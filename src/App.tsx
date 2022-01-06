0a57729b992d3c2112a357f9c60e7533d6d24224
Benjamin Millagou myagoo <millagou.benjamin@gmail.com>
2022-01-06 20:09:48 +0100

    zoom

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import React, { useEffect, useRef } from "react";
 import Victor from "victor";
 import "./App.css";
 
+const GRAVITATIONAL_CONSTANT = 1;
+const HERO_DENSITY = 10;
+const PLANET_DENSITY = 100;
+const BIG_PLANET_DENSITY = 1000;
+const PROJECTILE_DENSITY = 1;
+
+const ARTIFICIAL_RECOIL_CONSTANT = 1000;
+
 type Vector = {
   x: number;
   y: number;
 };
 
-const COLLIDER_MAP: Record<
+const BODY_MAP: Record<
   number,
   {
     color: string;
@@ -24,9 +32,9 @@ const getGravitationalForce = (
   mass2: number,
   distance: number
 ) => {
-  // const force = (0.5 * mass1 * mass2) / (distance * distance);
   const force =
-    (10 * mass1 * mass2) / (distance * Math.sqrt(distance) + 0.15);
+    (GRAVITATIONAL_CONSTANT * mass1 * mass2) /
+    (distance * Math.sqrt(distance) + 0.15);
   return force;
 };
 
@@ -38,6 +46,30 @@ const getDistance = (position1: Vector, position2: Vector) => {
 
 const getDirection = (position1: Vector, position2: Vector) => {
   return Math.atan2(position1.x - position2.x, position1.y - position2.y);
+};
+
+const CANVAS_ORIGIN = { x: 0, y: 0 }; // canvas origin
+
+let CANVAS_SCALE = 1; // current scale
+
+const scaleAt = (x: number, y: number, scaleBy: number) => {
+  // at pixel coords x, y scale by scaleBy
+  CANVAS_SCALE *= scaleBy;
+  CANVAS_ORIGIN.x = x - (x - CANVAS_ORIGIN.x) * scaleBy;
+  CANVAS_ORIGIN.y = y - (y - CANVAS_ORIGIN.y) * scaleBy;
+};
+
+const toWorld = ({ x, y }: Vector) => {
+  // convert to world coordinates
+  x = (x - CANVAS_ORIGIN.x) / CANVAS_SCALE;
+  y = (y - CANVAS_ORIGIN.y) / CANVAS_SCALE;
+  return { x, y };
+};
+
+const toScreen = ({ x, y }: Vector) => {
+  x = x * CANVAS_SCALE + CANVAS_ORIGIN.x;
+  y = y * CANVAS_SCALE + CANVAS_ORIGIN.y;
+  return { x, y };
 };
 
 const drawCircle = (
@@ -76,7 +108,7 @@ const drawBody = (
   body: RAPIER.RigidBody
 ) => {
   const position = body.translation();
-  const metadata = COLLIDER_MAP[collider.handle];
+  const metadata = BODY_MAP[body.handle];
   switch (collider.shapeType()) {
     case RAPIER.ShapeType.Ball: {
       drawCircle(ctx, position, collider.radius(), metadata.color);
@@ -99,12 +131,12 @@ const drawBody = (
       break;
     }
     case RAPIER.ShapeType.Cuboid: {
-      // console.log(collider.halfHeight());
       const halfExtents = collider.halfExtents();
+      const translation = collider.translation();
       drawCuboid(
         ctx,
-        position.x - halfExtents.x,
-        position.y - halfExtents.y,
+        translation.x - halfExtents.x,
+        translation.y - halfExtents.y,
         halfExtents.x * 2,
         halfExtents.y * 2,
         metadata.color,
@@ -136,7 +168,8 @@ const createPlanet = (
   vel: Vector,
   radius: number,
   density: number,
-  isStatic: boolean
+  isStatic: boolean,
+  color: string
 ) => {
   const planetBody = world.createRigidBody(
     new RAPIER.RigidBodyDesc(
@@ -149,12 +182,16 @@ const createPlanet = (
   const planetCollider = world.createCollider(
     RAPIER.ColliderDesc.ball(radius)
       .setDensity(density)
-      .setFriction(1)
-      .setRestitution(0)
+      .setFriction(0.9)
+      .setRestitution(0.1)
       .setActiveEvents(RAPIER.ActiveEvents.CONTACT_EVENTS),
     planetBody.handle
   );
 
+  BODY_MAP[planetBody.handle] = {
+    color,
+    positions: [],
+  };
   return [planetCollider, planetBody] as const;
 };
 
@@ -172,11 +209,44 @@ const createHero = (
   );
   const heroCollider = world.createCollider(
     RAPIER.ColliderDesc.cuboid(width / 2, height / 2)
-      .setDensity(10)
+      .setDensity(HERO_DENSITY)
+      .setFriction(1)
+      .setRestitution(0)
       .setActiveEvents(RAPIER.ActiveEvents.CONTACT_EVENTS),
     heroBody.handle
   );
+
+  BODY_MAP[heroBody.handle] = { color: "#FFFFFF", positions: [] };
+
   return [heroCollider, heroBody] as const;
+};
+
+const createProjectile = (world: RAPIER.World, pos: Vector, vel: Vector) => {
+  const rotation = Math.atan2(vel.y, vel.x);
+  const projectileBody = world.createRigidBody(
+    RAPIER.RigidBodyDesc.newDynamic()
+      .setTranslation(pos.x, pos.y)
+      .setLinvel(vel.x, vel.y)
+      .setRotation(rotation)
+  );
+
+  const projectileCollider = world.createCollider(
+    RAPIER.ColliderDesc.ball(3)
+      .setDensity(PROJECTILE_DENSITY)
+      .setFriction(0.5)
+      .setRestitution(0.5)
+      .setActiveEvents(RAPIER.ActiveEvents.CONTACT_EVENTS),
+
+    projectileBody.handle
+  );
+
+  BODY_MAP[projectileBody.handle] = {
+    isProjectile: true,
+    color: "#FF0000",
+    positions: [],
+  };
+
+  return [projectileCollider, projectileBody] as const;
 };
 
 function App() {
@@ -205,6 +275,7 @@ function App() {
 
     RAPIER.init().then(() => {
       let world = new RAPIER.World(new RAPIER.Vector2(0, 0));
+
       let eventQueue = new RAPIER.EventQueue(true);
 
       const heroHalfWidth = 10;
@@ -220,8 +291,6 @@ function App() {
         20
       );
 
-      COLLIDER_MAP[heroCollider.handle] = { color: "#FFFFFF", positions: [] };
-
       const KEYS = {
         LEFT: false,
         RIGHT: false,
@@ -236,8 +305,8 @@ function App() {
           x: mdEvent.offsetX,
           y: mdEvent.offsetY,
           radius: mdEvent.shiftKey
-            ? Math.random() * 40 + 40
-            : Math.random() * 10 + 10,
+            ? Math.random() * 500 + 500
+            : Math.random() * 50 + 50,
           color: "#" + Math.floor(Math.random() * 16777215).toString(16),
         };
 
@@ -250,9 +319,9 @@ function App() {
             throw new Error(`TEST`);
           }
 
-          const [planetCollider] = createPlanet(
+          createPlanet(
             world,
-            { x: tmp.x, y: tmp.y },
+            toWorld({ x: tmp.x, y: tmp.y }),
             mmPosition
               ? {
                   x: tmp.x - muEvent.offsetX,
@@ -260,14 +329,10 @@ function App() {
                 }
               : { x: 0, y: 0 },
             tmp.radius,
-            100,
-            mdEvent.ctrlKey
+            mdEvent.shiftKey ? BIG_PLANET_DENSITY : PLANET_DENSITY,
+            mdEvent.ctrlKey,
+            tmp.color
           );
-
-          COLLIDER_MAP[planetCollider.handle] = {
-            color: tmp.color,
-            positions: [],
-          };
 
           tmp = null;
           mmPosition = null;
@@ -280,6 +345,16 @@ function App() {
         canvas.addEventListener("mousemove", mmHandler);
 
         canvas.addEventListener("mouseup", muHandler);
+      });
+
+      canvas.addEventListener("wheel", (event) => {
+        console.log(event.deltaY);
+        const zoomBy = 1.1; // zoom in amount
+        scaleAt(
+          event.offsetX,
+          event.offsetY,
+          event.deltaY > 0 ? 1 / zoomBy : zoomBy
+        );
       });
 
       window.addEventListener("keydown", (event) => {
@@ -323,39 +398,41 @@ function App() {
             const heroHalfExtents = heroCollider.halfExtents();
             const heroRotation = heroCollider.rotation() + Math.PI / 2;
 
-            let rigidBodyDesc = RAPIER.RigidBodyDesc.newDynamic()
-              .setTranslation(
-                heroPosition.x +
+            const [, projectileBody] = createProjectile(
+              world,
+              {
+                x:
+                  heroPosition.x +
+                  heroHalfExtents.x +
                   Math.sin(-heroRotation) * heroHalfExtents.x * 2,
-                heroPosition.y + Math.cos(-heroRotation) * heroHalfExtents.y * 2
-              )
-              .setLinvel(
-                Math.sin(-heroRotation) * force,
-                Math.cos(-heroRotation) * force
-              );
+                y:
+                  heroPosition.y +
+                  Math.cos(-heroRotation) * heroHalfExtents.y * 2,
+              },
+              {
+                x: Math.sin(-heroRotation) * force,
+                y: Math.cos(-heroRotation) * force,
+              }
+            );
+
+            const projectileMass = projectileBody.mass();
 
             // Maybe use projectile mass for recoil
             heroBody.applyForce(
               new RAPIER.Vector2(
-                heroPosition.x - Math.sin(-heroRotation) * force * 100000,
-                heroPosition.y - Math.cos(-heroRotation) * force * 100000
+                heroPosition.x -
+                  Math.sin(-heroRotation) *
+                    force *
+                    projectileMass *
+                    ARTIFICIAL_RECOIL_CONSTANT,
+                heroPosition.y -
+                  Math.cos(-heroRotation) *
+                    force *
+                    projectileMass *
+                    ARTIFICIAL_RECOIL_CONSTANT
               ),
               true
             );
-            let projectileBody = world.createRigidBody(rigidBodyDesc);
-            const projectileCollider = world.createCollider(
-              RAPIER.ColliderDesc.ball(5)
-                .setDensity(1)
-                .setFriction(1)
-                .setRestitution(0.5)
-                .setActiveEvents(RAPIER.ActiveEvents.CONTACT_EVENTS),
-              projectileBody.handle
-            );
-            COLLIDER_MAP[projectileCollider.handle] = {
-              isProjectile: true,
-              color: "#FF0000",
-              positions: [],
-            };
 
             force = 0;
             break;
@@ -367,7 +444,11 @@ function App() {
 
       const ACCELERATION = 10000;
       const loop = () => {
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.setTransform(CANVAS_SCALE, 0, 0, CANVAS_SCALE, CANVAS_ORIGIN.x, CANVAS_ORIGIN.y);
 
         // TODO normalize vector when 2 arrow keys are pressed
         if (KEYS.UP) {
@@ -395,23 +476,28 @@ function App() {
             return;
           }
 
-          const metadata = COLLIDER_MAP[collider.handle];
-
           const bodyPosition = body.translation();
 
-          metadata.positions.unshift({
-            ...bodyPosition,
-          });
+          const bodyMass = body.mass();
 
-          if (metadata.positions.length > 10) {
-            metadata.positions.pop();
-          }
+          const alreadyComputedOtherBodies = new Set<number>();
 
           world.forEachCollider((otherCollider) => {
-            if (collider.handle === otherCollider.handle) {
+            const otherBodyHandle = otherCollider.parent();
+
+            if (body.handle === otherBodyHandle) {
               return;
             }
+
+            if (alreadyComputedOtherBodies.has(otherBodyHandle)) {
+              return;
+            } else {
+              alreadyComputedOtherBodies.add(otherBodyHandle);
+            }
+
             const otherBody = world.getRigidBody(otherCollider.parent());
+
+            const otherBodyMass = otherBody.mass();
 
             const otherBodyPosition = otherBody.translation();
 
@@ -422,20 +508,23 @@ function App() {
               bodyPosition
             );
             const forceMagnitude = getGravitationalForce(
-              otherBody.mass(),
-              body.mass(),
+              otherBodyMass,
+              bodyMass,
               distance
             );
-
-            body.applyForce(
-              new RAPIER.Vector2(
-                (Math.sin(forceDirection) * forceMagnitude) /
-                  Math.sqrt(body.mass()),
-                (Math.cos(forceDirection) * forceMagnitude) /
-                  Math.sqrt(body.mass())
-              ),
-              true
-            );
+            try {
+              body.applyForce(
+                new RAPIER.Vector2(
+                  (Math.sin(forceDirection) * forceMagnitude) /
+                    Math.sqrt(bodyMass),
+                  (Math.cos(forceDirection) * forceMagnitude) /
+                    Math.sqrt(bodyMass)
+                ),
+                true
+              );
+            } catch (error) {
+              console.error(error);
+            }
           });
         });
 
@@ -455,26 +544,28 @@ function App() {
 
         if (tmp) {
           const { x, y, radius, color } = tmp;
-          drawCircle(ctx, { x, y }, radius, color);
+          drawCircle(ctx, toWorld({ x, y }), radius, color);
 
           if (mmPosition) {
-            drawLine(ctx, { x, y }, mmPosition, color);
+            drawLine(ctx, toWorld({ x, y }), toWorld(mmPosition), color);
           }
         }
+
         world.step(eventQueue);
 
         eventQueue.drainContactEvents((handle1, handle2, started) => {
           if (!started) {
             return;
           }
-          const metaData1 = COLLIDER_MAP[handle1];
-          const metaData2 = COLLIDER_MAP[handle2];
 
           const collider1 = world.getCollider(handle1);
           const body1 = world.getRigidBody(collider1.parent());
 
           const collider2 = world.getCollider(handle2);
           const body2 = world.getRigidBody(collider2.parent());
+
+          const metaData1 = BODY_MAP[body1.handle];
+          const metaData2 = BODY_MAP[body2.handle];
 
           const collisionMagnitude = Victor.fromObject(body1.linvel())
             .subtract(Victor.fromObject(body2.linvel()))
@@ -484,13 +575,13 @@ function App() {
 
           if (collisionMagnitude > 200) {
             if (handle1 === heroCollider.handle && metaData2.isProjectile) {
-              COLLIDER_MAP[heroCollider.handle].color = "#FF0000";
+              BODY_MAP[heroBody.handle].color = "#FF0000";
               world.removeCollider(collider2, true);
               world.removeRigidBody(body2);
             }
 
             if (handle2 === heroCollider.handle && metaData1.isProjectile) {
-              COLLIDER_MAP[heroCollider.handle].color = "#FF0000";
+              BODY_MAP[heroBody.handle].color = "#FF0000";
               world.removeCollider(collider1, true);
               world.removeRigidBody(body1);
             }
@@ -508,7 +599,10 @@ function App() {
     <>
       <canvas tabIndex={1} ref={canvasRef} />
       <div className="overlay">
-        <span>Version 0.1</span>
+        <span>
+          Version 0.2 <a href="https://github.com/myagoo/graviwar">Github</a>
+        </span>
+        <span>Use mouse wheel to zoom in or out</span>
         <span>
           Move the box <kbd>&larr;</kbd> <kbd>&rarr;</kbd> <kbd>&uarr;</kbd>{" "}
           <kbd>&darr;</kbd>
