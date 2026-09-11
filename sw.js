8ad4c5383b19ca8ccddd01d2f4329e487d02bf5b
Benjamin Millagou <benjamin.millagou@dashdoc.com>
2026-09-11 11:35:23 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE = 'graviwar-1bcb12047868f152';
-const FILES = ["assets/home-logo-VmCoTE0b.png","assets/index-B3XAOUcb.js","assets/index-Bxv-bN5_.css","icons/apple-touch-icon-black.png","icons/favicon-32-black.png","icons/icon-192-black.png","icons/icon-512-black.png","icons/maskable-512-black.png","index.html","manifest.json"];
+const CACHE = 'graviwar-c8767238d1e3b8e9';
+const FILES = ["assets/home-logo-VmCoTE0b.png","assets/index-Dvow5yPR.js","assets/index-Owo86d6v.css","icons/apple-touch-icon-black.png","icons/favicon-32-black.png","icons/icon-192-black.png","icons/icon-512-black.png","icons/maskable-512-black.png","index.html","manifest.json"];
 const urls = FILES.map(file => new URL(file, self.registration.scope).href);
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(urls)));
